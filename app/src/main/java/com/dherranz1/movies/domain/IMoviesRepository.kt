98fd60547e5bf17c970494667acada07a4bf4707
dherranz1 <dweshh@proton.me>
2022-11-10 00:24:14 +0100

    Unnecesary use cases deleted Renamed Interface

--- app/src/main/java/com/dherranz1/movies/domain/IMoviesRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.dherranz1.movies.domain
-
-interface IMoviesRepository {
-    fun getAll() : List<MovieDomain>
-    fun get(id : String) : MovieDomain?
-    fun save(movie : MovieDomain)
-    fun saveAll(movieList : List<MovieDomain>)
-    fun delete(id : String)
-}
--- app/src/main/java/com/dherranz1/movies/domain/SaveMovieUseCase.kt
+++
@@ -1,6 +0,0 @@
-package com.dherranz1.movies.domain
-
-class SaveMovieUseCase(private val repository: IMoviesRepository) {
-    fun execute(movie : MovieDomain) =
-        repository.save(movie)
-}
