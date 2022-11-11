44bed619430bbdb8288563beb16b6a59fd19f160
dherranz1 <114154511+dherranz1@users.noreply.github.com>
2022-11-11 16:59:13 +0100

    Parameter renamed to more intuitive name

--- app/src/main/java/com/dherranz1/movies/data/MoviesDataRepository.kt
+++ app/src/main/java/com/dherranz1/movies/data/MoviesDataRepository.kt
@@ -1,19 +1,18 @@
 package com.dherranz1.movies.data
 
-import android.util.Log
 import com.dherranz1.movies.data.local.LocalDataSource
 import com.dherranz1.movies.data.remote.RemoteDataSource
 import com.dherranz1.movies.domain.MoviesRepository
 import com.dherranz1.movies.domain.MovieDomain
 
-class MoviesDataRepository(private val dataSource : LocalDataSource, private val remoteDataSource: RemoteDataSource) : MoviesRepository {
+class MoviesDataRepository(private val localDataSource : LocalDataSource, private val remoteDataSource: RemoteDataSource) : MoviesRepository {
 
     override fun getAll(): List<MovieDomain> {
-        var listado = dataSource.getAll()
+        var listado = localDataSource.getAll()
 
         if(listado.isEmpty()) {
             listado = remoteDataSource.getAll()
-            dataSource.saveAll(listado)
+            localDataSource.saveAll(listado)
         }
 
         return listado
@@ -21,12 +20,12 @@ class MoviesDataRepository(private val dataSource : LocalDataSource, private val
 
 
     override fun get(id: String): MovieDomain? {
-        var movie = dataSource.get(id)
+        var movie = localDataSource.get(id)
 
         movie?: run {
             movie = remoteDataSource.get(id)
             movie?.let {
-                dataSource.save(it)
+                localDataSource.save(it)
             }
         }
 
